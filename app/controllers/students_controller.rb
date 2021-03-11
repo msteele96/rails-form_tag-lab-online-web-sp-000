4a957e8b7f8bebeadbd05c79b448f5a4b1bdbf37
msteele96 <marcsteele2014@gmail.com>
2021-03-11 18:10:37 -0500

    commit tree 5b5630ab8fcaae2a7cfc506f0b6b82b6b64a5bca

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    Student.create(params[:student])
+    redirect_to new_student_path
+  end
+
 end
